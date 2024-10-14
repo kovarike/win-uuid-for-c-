23874e469f24c2703badbcab3ffa6b83d322afef
Kovarike <danilokovarike@gmail.com>
2024-10-14 13:39:22 -0300

    update version and script choco

--- scripts/build_choco.ps1
+++ scripts/build_choco.ps1
@@ -1,5 +1,5 @@
 $packageName = "winid"
-$version = "1.0.0"
+$version = $env:TAG_VERSION
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Cria o arquivo Nuspec
@@ -16,7 +16,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     <tags>uuid</tags>
     <docsUrl>https://github.com/kovarike/win-uuid-for-c-/wiki/winid</docsUrl>
     <bugTrackerUrl>https://github.com/kovarike/win-uuid-for-c-/issues</bugTrackerUrl>
-    <releaseNotes>https://github.com/kovarike/win-uuid-for-c-/releases</releaseNotes>
+    <releaseNotes>https://github.com/kovarike/win-uuid-for-c-/releases/tag/$version</releaseNotes>
     <projectUrl>https://github.com/kovarike/win-uuid-for-c-</projectUrl>
     <packageSourceUrl>https://github.com/kovarike/win-uuid-for-c-</packageSourceUrl>
     <licenseUrl>https://github.com/kovarike/win-uuid-for-c-/blob/main/LICENSE</licenseUrl>
@@ -24,9 +24,33 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 </package>
 "@ | Set-Content "$toolsDir\$packageName.nuspec"
 
+$rootDir = "$(Split-Path -Path $toolsDir -Parent)"
+
+# Verifica se o arquivo LICENSE.txt existe na raiz do projeto antes de copiá-lo
+$licenseFile = "$rootDir\LICENSE.txt"
+if (Test-Path $licenseFile) {
+    Copy-Item $licenseFile -Destination "$toolsDir\LICENSE.txt" -Force
+} else {
+    Write-Host "LICENSE.txt não encontrado na raiz do projeto."
+}
+
 # Cria o zip da biblioteca
 $zipPath = Join-Path $toolsDir "$packageName.zip"
 Compress-Archive -Path "$toolsDir\*" -DestinationPath $zipPath -Force
+
+# Cria o arquivo VERIFICATION.txt
+$sha256 = (Get-FileHash "$toolsDir\$packageName.zip" -Algorithm SHA256).Hash
+@"
+This package includes the following binary file:
+- $packageName.zip
+
+The binary was downloaded from the official repository:
+https://github.com/kovarike/win-uuid-for-c-
+
+To verify the integrity of the binary, compute the SHA256 hash and compare it with the following value:
+SHA256($packageName.zip): $sha256
+"@ | Set-Content "$toolsDir\VERIFICATION.txt"
+
 
 # Empacota para o Chocolatey
 choco pack $toolsDir\$packageName.nuspec
@@ -36,9 +60,4 @@ $package = "$PWD\$packageName.$version.nupkg"
 
 # Submeter o pacote para Chocolatey
 choco push $package --api-key=$apiKey --source="https://push.chocolatey.org/"
-
-
-
-
-
 
--- tools/README.md
+++ tools/README.md
@@ -1,4 +1,5 @@
 
+
 # Winid
 
 Esta biblioteca fornece funções para gerar UUIDs (Universally Unique Identifiers) em C. O objetivo é facilitar a criação e manipulação de UUIDs em suas aplicações.
@@ -6,7 +7,7 @@ Esta biblioteca fornece funções para gerar UUIDs (Universally Unique Identifie
 ## Exemplo de Uso
 
 - **Geração de UUIDs**: Cria UUIDs usando a função `create_uuid()`.
-- **Parsing de UUIDs**: Converte UUIDs para uma string formatada usando a função `parse()`.
+- **Parsing de UUIDs**: Copia o UUIDs para uma outra variavel usando a função `parse()`.
 
 ### 1. Utilizando o `parse` para copiar o ID gerado para outra variável
 
@@ -46,31 +47,40 @@ int main() {
 }
 
 ```
+## Versões 
 
+```bash
+https://github.com/kovarike/win-uuid-for-c-/releases/tag/v1.0.4
+```
 
 ## Instalação
 
 Para instalar a biblioteca `winid`, você pode usar o Chocolatey. Execute os seguintes comandos no terminal:
 
 ```bash
-# Instalar a versão 1.0.0
-choco install winid --version 1.0.0 -y
+# Instalar a versão 1.0.4
+choco install winid --version 1.0.4 -y
 
-# Atualizar para a versão 1.0.0
-choco upgrade winid --version=1.0.0
+# Atualizar para a versão 1.0.4
+choco upgrade winid --version=1.0.4
 
-# Desinstalar a versão 1.0.0
-choco uninstall winid --version=1.0.0
+# Desinstalar a versão 1.0.4
+choco uninstall winid --version=1.0.4
 ```
 
-## Clonar o Repositório
+## Clonar o Repositório ou Baixar o Arquivo zip
 
-Se preferir, você pode clonar o repositório diretamente
+Se preferir, você pode clonar o repositório ou baixar o arquivo zip contendo as biblioteca e cabeçalho para ser utilizado.
 
 ```bash
 git clone https://github.com/kovarike/win-uuid-for-c.git
+```
+```bash
+https://github.com/kovarike/win-uuid-for-c-/releases/tag/v1.0.4
 ```
 
 ### Licença
 
 Este projeto é licenciado sob a [**MIT License**](./LICENSE). Você pode usar, copiar, modificar e distribuir este software, desde que mantenha os avisos de copyright e de permissão nas cópias ou partes substanciais do software. O software é fornecido "como está", sem garantia de qualquer tipo, expressa ou implícita.
+
+<!-- git tag -a v1.0.0 -m "Release version 1.0.0"   -->
